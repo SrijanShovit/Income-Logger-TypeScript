e02512c3fccd8e29b0200e54b3a54b8fab247f6f
Srijan Shovit <srijanshovit@gmail.com>
2021-12-01 16:20:44 +0530

    dynamic

--- sandbox.ts
+++ sandbox.ts
@@ -1,45 +1,13 @@
-//we can without defining a value for a variable explcitily define its type
-//telling TS that is future it'll accept values of this particular type only
+let age:any =25  //this variable chan be changed to diff types
+age = true
+console.log(age)
 
-let character:string ;
-let age:number
-let isLoggedIn:boolean
-
-//age = 'luigi
-age = 30
-
-//isLoggedIn = 25
-isLoggedIn = true
-
-//arrays
-let ninjas:string[]  //still not empty array so won't allow push back
-ninjas = []
-ninjas.push('shaun')
+let mixed:any[] = []
+mixed.push(5)
+mixed.push('mario')
+mixed.push(false)
 
 
-//union types for mixed data types
-let mixed:(string|number)[] = []
-mixed.push(20)
-// mixed.push(isLoggedIn)
-mixed.push('20')
-
-//uinion types to normal variable
-let uid:string|number
-uid = '123'
-uid = 123
-
-
-//objects
-let ninjaOne :object
-ninjaOne = {name: 'yoshi',age:30}
-//array is allowed bcz array is also a type of subject
-ninjaOne = ['Two']
-
-//strictly defining to be object
-let ninjaTwo : {
-    name: string,
-    age:number,
-    beltColor: string
-}
-
-// ninjaTwo = {}
+let ninja: {name:any, age:any}
+ninja = {name: 'mario', age:23}
+ninja = {name: false, age:'mario'}
