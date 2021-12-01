394dffc8e63c1850de2e6ef6696abae31b63537f
Srijan Shovit <srijanshovit@gmail.com>
2021-12-01 17:58:45 +0530

    classes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,27 @@
 "use strict";
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on mario website', 250);
+const invTwo = new Invoice('luigi', 'work on luigi website', 350);
+console.log(invOne);
+//array which will have objects only made out of  Invoice class
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+invOne.amount = 400;
+invOne.client = 'mariola';
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
-// console.log(form.children)
 //inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,38 @@
+//classes
+class Invoice{
+    client:string;
+    details:string;
+    amount:number;
+
+    constructor(c:string,d:string,a:number){
+        this.client =c;
+        this.details =d;
+        this.amount =a;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario','work on mario website',250);
+const invTwo = new Invoice('luigi','work on luigi website',350);
+
+console.log(invOne)
+
+//array which will have objects only made out of  Invoice class
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+console.log(invoices)
+
+invOne.amount = 400
+invOne.client = 'mariola'
+console.log(invoices)
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children)
+
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
