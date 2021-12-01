79f5505a87682ef000e83d9b42f06e2277ab9b7f
Srijan Shovit <srijanshovit@gmail.com>
2021-12-01 16:48:31 +0530

    type alias

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,24 +1,10 @@
 "use strict";
-let greet;
-//greet = 'hello'
-greet = () => {
-    console.log("hello, again");
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-//c is  optional parameter
-//d has a default value so may or may not be passed
-///always put required parameters first otherwise while passing they'll get mixed up
-const add = (a, b, c, d = false) => {
-    console.log(a + b);
-    // console.log(a+'b')
-    console.log(c); //undefined
-    console.log(d); //false
+const greet = (user) => {
+    console.log(`${user.name} greets`);
 };
-add(5, 10);
-//TS automatically refers return type of a function and set that type to assigned variable
-//We may or may not specify rteurn type pf function
-//if function has no return type then by default void is returned
-const minus = (a, b) => {
-    return a - b;
+const greetAgain = (user) => {
+    console.log(`${user.name} greets`);
 };
-let result = minus(5, 10);
-// result = 'result'
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,29 +1,14 @@
-let greet:Function
+type StrOrNum = string | number;
+type Obj = {name: string, uid:StrOrNum}
 
-//greet = 'hello'
-
-greet = () => {
-    console.log("hello, again")
+const logDetails = (uid: StrOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-
-//c is  optional parameter
-//d has a default value so may or may not be passed
-///always put required parameters first otherwise while passing they'll get mixed up
-const add = (a:number, b:number,c?:number | string,d:boolean = false) => {
-    console.log(a+b)
-    // console.log(a+'b')
-    console.log(c)            //undefined
-    console.log(d)             //false
+const greet = (user:Obj) => {
+    console.log(`${user.name} greets`)
 }
-add(5,10)
 
-
-//TS automatically refers return type of a function and set that type to assigned variable
-//We may or may not specify rteurn type pf function
-//if function has no return type then by default void is returned
-const minus = (a:number,b:number):number =>{
-    return a-b;
+const greetAgain = (user:Obj) => {
+    console.log(`${user.name} greets`)
 }
-let result = minus(5,10);
-// result = 'result'
