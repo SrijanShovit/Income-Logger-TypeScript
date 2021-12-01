c6935336d024537bcb20b99bef903a75c2083a7c
Srijan Shovit <srijanshovit@gmail.com>
2021-12-01 18:05:33 +0530

    access

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 "use strict";
 //classes
+//public is default behaviour of things associated with class
 class Invoice {
     constructor(c, d, a) {
         this.client = c;
@@ -12,15 +13,12 @@ class Invoice {
 }
 const invOne = new Invoice('mario', 'work on mario website', 250);
 const invTwo = new Invoice('luigi', 'work on luigi website', 350);
-console.log(invOne);
-//array which will have objects only made out of  Invoice class
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-console.log(invoices);
-invOne.amount = 400;
-invOne.client = 'mariola';
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
 //inputs
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 //classes
+//public is default behaviour of things associated with class
 class Invoice{
-    client:string;
-    details:string;
-    amount:number;
+    readonly client:string;   //readonly can be read from anywhere but changed from nowhere;not even class
+    private details:string;   //private can be only within the class
+    public amount:number;
 
     constructor(c:string,d:string,a:number){
         this.client =c;
@@ -18,17 +19,14 @@ class Invoice{
 const invOne = new Invoice('mario','work on mario website',250);
 const invTwo = new Invoice('luigi','work on luigi website',350);
 
-console.log(invOne)
-
-//array which will have objects only made out of  Invoice class
 let invoices: Invoice[] = []
 invoices.push(invOne)
 invoices.push(invTwo)
-console.log(invoices)
 
-invOne.amount = 400
-invOne.client = 'mariola'
-console.log(invoices)
+
+invoices.forEach(inv => {
+    console.log(inv.client,inv.amount,inv.format())
+})
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
